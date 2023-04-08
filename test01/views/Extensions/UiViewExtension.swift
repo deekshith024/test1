808b9d20508a64cb3be9e8e1b7277f174e6b914b
deekshith024 <105255904+deekshith024@users.noreply.github.com>
2023-04-08 13:35:16 -0400

    Add files via upload

---
+++ test01/views/Extensions/UiViewExtension.swift
@@ -0,0 +1,20 @@
+//
+//  UiViewExtension.swift
+//  test01
+//
+//  Created by deekshith
+//
+
+import UIKit
+
+
+extension UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        
+            get { return cornerRadius}
+            set {
+                self.layer.cornerRadius = newValue
+            }
+    }
+}
